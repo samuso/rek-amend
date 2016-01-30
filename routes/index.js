f57e4b68188799a815ee5e933addcf451854bd18
mombi93 <m.d.a.rafdi@gmail.com>
2016-01-30 14:56:23 +0000

    get HOD to work yay mudafakas! :D

--- routes/index.js
+++ routes/index.js
@@ -2,20 +2,20 @@ var express = require('express');
 var router = express.Router();
 var request = require('request');
 var havenondemand = require('havenondemand')
-var hodClient = new havenondemand.HODClient('https://api.havenondemand.com/1/api/sync/analyzesentiment/v1', 'c643526f-4611-49da-a4bd-05a4d5690336');
+var hodClient = new havenondemand.HODClient('c643526f-4611-49da-a4bd-05a4d5690336', 'v1')
 
 router.get('/', function(req, res) {
   res.render('index', { title: 'rek-amend', content: 'none' });
 });
 
 router.get('/text',function(req,res) {
-    var text = 'Hack cambridge never fails to dissapoint. I am the best ever! This is positive'
-    hodClient.call('analyzesentiment', text, function(err, hodRes){
+    var data = {text: 'Hack cambridge never fails to dissapoint. I am the best ever! This is positive'};
+    hodClient.call('analyzesentiment', data, function(err, hodRes, hodBody){
         //var sentiment = res.body.aggregate.sentiment
         //var score = res.body.aggregate.score
         //console.log('------------------------------');
         //console.log(text + ' | ' + sentiment + ' | ' + score);
-        res.json({ _shiz: hodRes.body});
+        res.json({ _shiz: hodBody});
     });
 });
 

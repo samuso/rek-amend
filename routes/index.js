3c7a3dd805459e4145c203c247c4c943b738a672
mombi93 <m.d.a.rafdi@gmail.com>
2016-01-30 14:49:17 +0000

    added HOM module

--- routes/index.js
+++ routes/index.js
@@ -1,22 +1,21 @@
 var express = require('express');
 var router = express.Router();
 var request = require('request');
+var havenondemand = require('havenondemand')
+var hodClient = new havenondemand.HODClient('https://api.havenondemand.com/1/api/sync/analyzesentiment/v1', 'c643526f-4611-49da-a4bd-05a4d5690336');
 
 router.get('/', function(req, res) {
   res.render('index', { title: 'rek-amend', content: 'none' });
 });
 
 router.get('/text',function(req,res) {
-    request.get({
-        url: "https://api.havenondemand.com/1/api/sync/analyzesentiment/v1",
-        apikey: "c643526f-4611-49da-a4bd-05a4d5690336",
-        text: "Hack cambridge never fails to dissapoint. I am the best ever! This is positive"
-    }, function(err, httpResponse, body){
-        if(err) console.log(err);
-        console.log(httpResponse);
-        console.log(body);
-        res.json({ _httpResponse: httpResponse, _body: body });
-
+    var text = 'Hack cambridge never fails to dissapoint. I am the best ever! This is positive'
+    hodClient.call('analyzesentiment', text, function(err, hodRes){
+        //var sentiment = res.body.aggregate.sentiment
+        //var score = res.body.aggregate.score
+        //console.log('------------------------------');
+        //console.log(text + ' | ' + sentiment + ' | ' + score);
+        res.json({ _shiz: hodRes.body});
     });
 });
 

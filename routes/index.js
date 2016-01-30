0df4b1739b9702c1e4785b13921e159c56c1f492
mombi93 <m.d.a.rafdi@gmail.com>
2016-01-30 14:27:50 +0000

    added some routes

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,23 @@
 var express = require('express');
 var router = express.Router();
+var request = require('request');
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'rek-amend' });
+router.get('/', function(req, res) {
+  res.render('index', { title: 'rek-amend', content: 'none' });
+});
+
+router.get('/text',function(req,res) {
+    request.get({
+        url: "https://api.havenondemand.com/1/api/sync/analyzesentiment/v1",
+        apikey: "c643526f-4611-49da-a4bd-05a4d5690336",
+        text: "Hack cambridge never fails to dissapoint. I am the best ever! This is positive"
+    }, function(err, httpResponse, body){
+        if(err) console.log(err);
+        console.log(httpResponse);
+        console.log(body);
+        res.json({ _httpResponse: httpResponse, _body: body });
+
+    });
 });
 
 module.exports = router;
